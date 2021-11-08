8736d3ee65fd44c11f80b2bee4c1e494b7e72731
Lesly San Luis Celaya <lesly.sanluis@gmail.com>
2021-11-07 23:23:29 -0600

    bd_product

--- pineappleapp-data-jpa/src/main/java/com/pineapple/springboot/app/models/entity/Product.java
+++ pineappleapp-data-jpa/src/main/java/com/pineapple/springboot/app/models/entity/Product.java
@@ -1,5 +1,102 @@
 package com.pineapple.springboot.app.models.entity;
 
-public class Product {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product implements Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private	String name;
+	private	String category;
+	private	String description;
+	private	String quantity;
+	private String price;
+	private String picture;
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getCategory() {
+		return category;
+	}
+
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public String getQuantity() {
+		return quantity;
+	}
+
+
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+
+
+	public String getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+
+	public String getPicture() {
+		return picture;
+	}
+
+
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+
+	private static final long serialVersionUID = 1L;
 
 }
