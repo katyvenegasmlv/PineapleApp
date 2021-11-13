42258df043a72aa53e5810bfe79397f8547a1d0e
hackerrank <me@hackerrank.com>
2021-11-13 20:38:54 -0300

    api rest product

--- pineapplemarket/src/main/java/com/pineapple/pineapplemarket/Controller/IProduct.java
+++
@@ -1,7 +0,0 @@
-package com.pineapple.pineapplemarket.Controller;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface IProduct extends CrudRepository<com.pineapple.Clases.Product, Long> {
-    
-}
---
+++ pineapplemarket/src/main/java/com/pineapple/pineapplemarket/productRest.java
@@ -0,0 +1,49 @@
+package com.pineapple.pineapplemarket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.pineapple.Clases.Product;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(path = "/product")
+@CrossOrigin(origins = "http://localhost")
+public class productRest {
+
+    ///Metodo que va devolver una lista de productos
+    @GetMapping("/GetProducts")
+    public List<Product> GetProducts(){
+
+        List<Product> products = new ArrayList<Product>();
+
+
+        return products;
+    } 
+
+    ///Metodo que va insertar 1 producto
+    @GetMapping("/InsertProduct")
+    public void InsertProduct(Product product){
+
+    } 
+
+    @GetMapping("/UpdateProduct")
+    public void UpdateProduct(Product product){
+
+    } 
+
+    @GetMapping("/GetProduct")
+    public void GetProduct(String name){
+
+    } 
+
+    @GetMapping("/DeleteProduct")
+    public void DeleteProduct(String name){
+
+    } 
+}
